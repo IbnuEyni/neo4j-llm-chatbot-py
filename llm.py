c48efb7a2ca4c85e6499df45c3137bba1607d05e
IbnuEyni <shuaibahmedin@gmail.com>
2024-11-04 13:46:35 +0300

    create the llm

---
+++ llm.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+import streamlit as st
+from graph import graph
+
+load_dotenv()
+
+# Create the LLM
+llm =  ChatGoogleGenerativeAI(
+        model="gemini-1.5-pro",
+        temperature=0, 
+        max_tokens=None,
+        timeout=None,
+        google_api_key = os.getenv('GEMINI_API_KEY')
+    )
+
